b1017a8126ebe761a613a5cd247d1fc313a89798
Jeremy Rimpo <jeremy.rimpo@servermonkey.com>
2022-10-10 13:48:32 -0500

    Updates for errors

--- app/controllers/redmine_oauth_controller.rb
+++ app/controllers/redmine_oauth_controller.rb
@@ -19,70 +19,104 @@ class RedmineOauthController < AccountController
       flash['error'] = l(:notice_access_denied)
       redirect_to signin_path
     else
-      token = oauth_client.auth_code.get_token(params['code'], :redirect_uri => oauth_azure_callback_url, :resource => "00000002-0000-0000-c000-000000000000")
+      token = nil
+      begin
+        token = oauth_client.auth_code.get_token(params['code'], :redirect_uri => oauth_azure_callback_url, :resource => "00000002-0000-0000-c000-000000000000")
+      rescue OAuth2::Error => e
+        flash[:error] = l(:notice_unable_to_obtain_azure_credentials) + " " + e.description
+        redirect_to signin_path
+        return
+      end
       user_info = JWT.decode(token.token, nil, false)
       logger.error user_info
-      
+
       email = user_info.first['unique_name']
 
-      if email
-        checked_try_to_login email, user_info.first
+      if Redmine::VERSION.to_s.starts_with?('2.')
+        user = User.where(:mail => email).first
       else
-        flash['error'] = l(:notice_no_verified_email_we_could_use)
-        redirect_to signin_path
+        user = User.joins(:email_addresses).where(:email_addresses => { :address => email }).first
+      end
+
+      if user
+        checked_try_to_login user.mail, user_info.first, user
+      else
+        if email
+          user = User.new
+          checked_try_to_login email, user_info.first, user
+        else
+          flash['error'] = l(:notice_no_verified_email_we_could_use)
+          redirect_to signin_path
+        end
       end
     end
   end
 
-  def checked_try_to_login(email, user)
+  def checked_try_to_login(email, info, user)
     if allowed_domain_for?(email)
-      try_to_login email, user
+      try_to_login email, info, user
     else
-      flash['error'] = l(:notice_domain_not_allowed, :domain => parse_email(email)['domain'])
+      flash[:error] = l(:notice_domain_not_allowed, :domain => parse_email(email)[:domain])
       redirect_to signin_path
     end
   end
 
-  def try_to_login email, info
-    params['back_url'] = session['back_url']
+  def try_to_login email, info, user
+    params[:back_url] = session[:back_url]
     session.delete(:back_url)
 
-    user = User.joins(:email_addresses)
-               .where('email_addresses.address' => email, 'email_addresses.is_default' => true)
-               .first_or_initialize
-    if user.new_record?
-      # Self-registration off
-      redirect_to(home_url) && return unless Setting.self_registration?
-      # Create on the fly
-      user.firstname = info['given_name'] unless info['given_name'].nil?
-      user.lastname = info['family_name'] unless info['family_name'].nil?
-      user.firstname ||= info["name"]
-      user.lastname ||= info["name"]
-      user.mail = email
-      user.login = email
-      user.random_password
-      user.register
-
-      case Setting.self_registration
-      when '1'
-        register_by_email_activation(user) do
-          onthefly_creation_failed(user)
-        end
-      when '3'
-        register_automatically(user) do
-          onthefly_creation_failed(user)
-        end
+    @user = user
+    params = {}
+    params["firstname"] = info["given_name"] unless info["given_name"].nil?
+    params["lastname"] = info["family_name"] unless info["family_name"].nil?
+    params["firstname"] ||= info["name"]
+    params["lastname"] ||= info["name"]
+    params["mail"] = email
+    params["login"] = email
+    logger.error login
+    checkuser = User.find_by_login(login)
+    logger.error checkuser
+    if checkuser
+      # Existing record
+      if checkuser.active?
+        checkuser.update_column(:last_login_on, Time.now)
+        successful_authentication(checkuser)
       else
-        register_manually_by_administrator(user) do
-          onthefly_creation_failed(user)
-        end
+        account_pending(checkuser)
       end
     else
-      # Existing record
-      if user.active?
-        successful_authentication(user)
+      if @user.new_record?
+        # Self-registration off
+        redirect_to(home_url) && return unless Setting.self_registration?
+        # Create on the fly
+        @user.login = login
+        @user.safe_attributes = params
+        @user.admin = false
+        @user.random_password
+        @user.register
+
+        case Setting.self_registration
+        when '1'
+          register_by_email_activation(@user) do
+            onthefly_creation_failed(@user)
+          end
+        when '3'
+          register_automatically(@user) do
+            onthefly_creation_failed(@user)
+          end
+        else
+          register_manually_by_administrator(@user) do
+            onthefly_creation_failed(@user)
+          end
+        end
       else
-        account_pending
+        # Existing record
+        if @user.active?
+          @user.update_column(:last_login_on, Time.now)
+          successful_authentication(@user)
+        else
+          account_pending(@user)
+        end
       end
     end
   end
